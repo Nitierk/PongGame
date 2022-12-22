c466eb9d1944c77765b961a8f136fff6df2e8f97
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-12-22 08:43:10 -0300

    Feat: html e campo feito

---
+++ script/js.js
@@ -0,0 +1,33 @@
+
+
+window.onload = function() {
+    setInterval(execucacao, 1000 / 30)
+}
+var xBola = yBola = 30
+function execucacao() { 
+    var folhaDesenho = document.getElementById("folha");
+    var areaDesenho = folhaDesenho.getContext("2d");
+    
+    var wCampo = 700
+    var hCampo = 500
+    
+    var wRede = 5
+    var hRede = 500
+    
+    var hRaquete = 80
+    var yRaquete = 200
+    
+    var diametroBola = 10
+    
+    areaDesenho.fillStyle = '#286047'
+    areaDesenho.fillRect(0, 0, wCampo, hCampo) // Campo
+    
+    areaDesenho.fillStyle = 'white'
+    areaDesenho.fillRect(wCampo/2 - wRede/2, 0, wRede, hRede) // Rede
+    areaDesenho.fillRect(2, yRaquete, 10, hRaquete) // Raquete 1 
+    areaDesenho.fillRect(687, 200, 10, 80) // Raquete 2
+    areaDesenho.fillRect(xBola - diametroBola/2, yBola - diametroBola/2, diametroBola, diametroBola) // bola 
+
+    xBola += 1
+    yBola += 1
+}
